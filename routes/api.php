6e8c6f4c4a9c8e85eaf7a83ec49a8dfa1a2173c1
Filip <filipivanovic59@gmail.com>
2024-09-19 10:23:43 +0200

    Post Model and Controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 */
 
-Route::get('/',function(){
-    return "API";
-});
+Route::apiResource('posts',PostController::class);
+
