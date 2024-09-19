fa797696c5fb4702c9a713ab52d65a316f0e1531
Filip <filipivanovic59@gmail.com>
2024-09-19 14:13:18 +0200

    Authorization

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,7 +12,14 @@ class Post extends Model
     protected $fillable = [
          'title',
         'body'
-    ]
+    ];
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+    }
+
+
        
     
 }
